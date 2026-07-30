d8d139c58ba7ce4a75ec5d043686571df98a642e
Vasiliy <91281754+vasyasgors@users.noreply.github.com>
2026-07-30 16:14:45 +0300

    Update PostProcessBuild.cs

--- Editor/PostProcessBuild.cs
+++ Editor/PostProcessBuild.cs
@@ -8,9 +8,11 @@ using System.IO.Compression;
 
 public class PostProcessBuild 
 {
-    //[PostProcessBuild]
+    /*
+    [PostProcessBuild]
     public static void CreateZip(BuildTarget target, string pathToBuiltProject)
     {
+        
         if (target == BuildTarget.WebGL)
         {
             string version = PlayerSettings.bundleVersion;
@@ -25,6 +27,6 @@ public class PostProcessBuild
 
             ZipFile.CreateFromDirectory(pathToBuiltProject, archivePath);
         }
-    }
+    } */
 
 }
